df92176700ab6921c63b18112238092effb19cd9
Domenic Murtari <domenic.murtari@gmail.com>
2016-03-11 10:00:53 -0700

    Sensor trips twice, so only count a revolution every 2 triggers

--- motion.py
+++ motion.py
@@ -10,6 +10,8 @@ io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)
 
 count = 0
 total_inches = 0
+revolutions = 0
+wheel_circumference = 33
 
 def convert(inches):
     feet = float(inches) / 12.0
@@ -22,12 +24,14 @@ def convert(inches):
 while True:
     if not io.input(door_pin):
         if not sensor_triggered:
-            total_inches += 33
-            print("Sensor closed", count)
-            print("Total distance", convert(total_inches))
+            print count
+            if count % 2 == 0:
+                revolutions += 1
+                total_inches += wheel_circumference 
+                print("Total revolutions", revolutions)
+                print("Total distance", convert(total_inches))
             sensor_triggered = True
             count += 1
     if io.input(door_pin):
         if sensor_triggered:
             sensor_triggered = False
-            count += 1
